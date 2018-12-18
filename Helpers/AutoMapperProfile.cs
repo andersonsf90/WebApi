b95253c7a69d32109833c3a8756f6deeba9e9a94
anderson.souza <anderson.souza@agu.gov.br>
2018-12-18 17:35:24 -0200

    first commit

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebApi.Dtos;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
