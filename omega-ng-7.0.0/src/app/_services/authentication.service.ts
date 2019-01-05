0d4949eb89109ae316c5ad5083ce3d3bcbc2680e
andersonsf90 <anderson.souza.freitas@gmail.com>
2019-01-05 16:52:38 -0200

    APIURL no enviroment.ts

--- omega-ng-7.0.0/src/app/_services/authentication.service.ts
+++ omega-ng-7.0.0/src/app/_services/authentication.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { environment } from 'environments/environment';
 import { User } from '@/_models';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
+    public baseUrl = environment.baseUrl;
 
     constructor(private http: HttpClient) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
@@ -20,7 +22,7 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
+        return this.http.post<any>(this.baseUrl + '/users/authenticate', { username, password })
             .pipe(map(user => {
                 // login successful if there's a jwt token in the response
                 if (user && user.token) {
--- omega-ng-7.0.0/src/app/_services/user.service.ts
+++ omega-ng-7.0.0/src/app/_services/user.service.ts
@@ -1,29 +1,33 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'environments/environment';
 import { User } from '@/_models';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
+
+    public baseUrl = environment.baseUrl;
+
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${config.apiUrl}/users`);
+        return this.http.get<User[]>(this.baseUrl + '/users');
     }
 
     getById(id: number) {
-        return this.http.get(`${config.apiUrl}/users/${id}`);
+        return this.http.get(this.baseUrl + '/users/${id}');
     }
 
     register(user: User) {
-        return this.http.post(`${config.apiUrl}/users/register`, user);
+        return this.http.post(this.baseUrl + '/users/register', user);
     }
 
     update(user: User) {
-        return this.http.put(`${config.apiUrl}/users/${user.id}`, user);
+        return this.http.put(this.baseUrl + '/users/${user.id}', user);
     }
 
     delete(id: number) {
-        return this.http.delete(`${config.apiUrl}/users/${id}`);
+        return this.http.delete(this.baseUrl + '/users/${id}');
     }
 }
